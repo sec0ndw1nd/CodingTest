7e7f9da5ec8557a53406e200067228bdb7872f41
sec0ndw1nd <141013318+sec0ndw1nd@users.noreply.github.com>
2024-08-13 20:50:30 +0900

    [level 1] Title: 약수의 합, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    if (n >= 0 && n < 2) return n;
+    
+    const resultArr = [];
+    const sqrtN = Math.sqrt(n);
+    const centerN = Math.floor(sqrtN);
+    const isOddLength = sqrtN !== centerN;
+    
+    for (let i = 1; i <= centerN; i++) {
+        if (!(n % i)) resultArr.push(i);
+    }
+    
+    const rest = [...resultArr];
+    if (resultArr.at(-1) === sqrtN) rest.pop();
+    return resultArr.concat(rest.reverse().map((v) => n / v)).reduce((acc, curr) => acc += curr);
+}
